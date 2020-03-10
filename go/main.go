2a0633b8a79fc1fd0227bcc12c3a8e3f7ec28be2
yaozeyuan <yaozeyuan001@lianjia.com>
2020-03-10 18:04:10 +0800

    feat: 添加demo配置

--- go/main.go
+++ go/main.go
@@ -9,10 +9,10 @@ import (
 type RequestParams struct {
 	DisplayName string `json:"display_name"`
 	PackageName string `json:"package_name"`
-	// DevListJSON string `json:"dev_list_json"`
-	// Description string `json:"description"`
-	// SiteURL     string `json:"site_url"`
-	// Remark      string `json:"remark"`
+	DevListJSON string `json:"dev_list_json"`
+	Description string `json:"description"`
+	SiteURL     string `json:"site_url"`
+	Remark      string `json:"remark"`
 }
 
 func main() {
@@ -24,16 +24,18 @@ func main() {
 	// 此处必须使用引用
 	test := reflect.TypeOf(&a)
 	test1 := test.Elem()
-	test2, _ := test1.FieldByName("DisplayName")
+	test2, _ := test1.FieldByName("DevListJSON")
 	test3 := test2.Tag
-	goType := test2.Type       // 可以拿到数据类型string
-	test4 := test3.Get("json") // 可以拿到json标记对应的值: display_name
+	goType := test2.Type              // 可以拿到数据类型string
+	test4 := test3.Get("json")        // 可以拿到json标记对应的值: display_name
+	notExist := test3.Get("notExist") // 可以拿到json标记对应的值: display_name
 	fmt.Println("tag => ", test)
 	fmt.Println("tag => ", test1)
 	fmt.Println("tag => ", test2)
 	fmt.Println("tag => ", test3)
 	fmt.Println("goType => ", goType)
 	fmt.Println("tag => ", test4)
+	fmt.Println("notExist tag => ", notExist)
 	return
 	// 	test1 := test.Elem()
 	// 	test2, _ := test1.FieldByName("DisplayName")
