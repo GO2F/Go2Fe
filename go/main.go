b2298f2f34f78c937053bc44cb18d83359c67981
yaozeyuan <yaozeyuan001@lianjia.com>
2020-03-14 00:09:14 +0800

    feat: go部分完成, 开始做手实现前端代码部分

--- go/main.go
+++ go/main.go
@@ -1,52 +1,8 @@
 package main
 
-import (
-	// "fmt"
-	// "reflect"
-	"go2fe/generate"
-)
-
-// RequestParams test
-type RequestParams struct {
-	DisplayName string `json:"display_name"`
-	PackageName string `json:"package_name"`
-	DevListJSON string `json:"dev_list_json"`
-	Description string `json:"description"`
-	SiteURL     string `json:"site_url"`
-	Remark      string `json:"remark"`
-}
+import "go2fe/test"
 
 func main() {
-
-	generate.GetJSONConfig()
-	generate.WriteConfig()
-
-	// a := RequestParams{
-	// 	DisplayName: "123123",
-	// 	PackageName: "123",
-	// }
-
-	// // 此处必须使用引用
-	// test := reflect.TypeOf(&a)
-	// test1 := test.Elem()
-	// test2, _ := test1.FieldByName("DevListJSON")
-	// test3 := test2.Tag
-	// goType := test2.Type              // 可以拿到数据类型string
-	// test4 := test3.Get("json")        // 可以拿到json标记对应的值: display_name
-	// notExist := test3.Get("notExist") // 可以拿到json标记对应的值: display_name
-	// fmt.Println("tag => ", test)
-	// fmt.Println("tag => ", test1)
-	// fmt.Println("tag => ", test2)
-	// fmt.Println("tag => ", test3)
-	// fmt.Println("goType => ", goType)
-	// fmt.Println("tag => ", test4)
-	// fmt.Println("notExist tag => ", notExist)
+	test.Run()
 	return
-	// 	test1 := test.Elem()
-	// 	test2, _ := test1.FieldByName("DisplayName")
-	// 	test3 := test2.Tag
-	// 	tag := test3.Get("json")
-	// 	fmt.Println("tag => ", tag)
-	// 	fmt.Println("hello world!\n")
-	// 	fmt.Println(os.Args)
 }
